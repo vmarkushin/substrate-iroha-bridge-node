ced4365bad6fc21530e4affdb6459d14f105f664
Vladislav Markushin <negigic@gmail.com>
2020-08-20 12:32:39 +0300

    Remove `docker-compose build` in run scripts

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -7,5 +7,4 @@ echo "*** Start Substrate Iroha Bridge ***"
 cd $(dirname ${BASH_SOURCE[0]})/../../..
 
 docker-compose -f docker-compose-substrate.yml down --remove-orphans
-docker-compose -f docker-compose-substrate.yml build dev-manual
 docker-compose -f docker-compose-substrate.yml run --rm --service-ports dev-manual $@
